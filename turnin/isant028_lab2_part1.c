9ee9e59acbeec8a0905768572df1cc42f7a3f6c5
Isabella Santiago <isant028@bolt.cs.ucr.edu>
2021-04-10 17:21:06 -0700

    finish part 1

--- turnin/isant028_lab2_part1.c
+++ turnin/isant028_lab2_part1.c
@@ -16,21 +16,24 @@ int main(void) {
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-while(1) {
-		// 1) Read input
-		tmpA = PINA & 0x01;
-		// 2) Perform computation
-		// if PA0 is 1, set PB1PB0 = 01, else = 10
-		if (tmpA == 0x01) { // True if PA0 is 1
-			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
-							 // (clear rightmost 2 bits, then set to 01)
-		} else {
-			tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
-							 // (clear rightmost 2 bits, then set to 10)
-		}	
-	// 3) Write output
-PORTB = tmpB;	
+	while(1) {
+		tmpA = PINA & 0x03;
+		if (tmpA == 0x00) {
+			tmpB = 0x00;
+		}
+                else if (tmpA == 0x01) {
+                       tmpB = 0x01;
+                }
+                else if (tmpA == 0x02) {
+                        tmpB = 0x00;
+                }
+                else if (tmpA == 0x03) {
+                       tmpB = 0x00;
+                }
+
+		PORTB = tmpB;
+
 	}
 	return 0;
-}
 
+}
